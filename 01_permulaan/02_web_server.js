0566b6784c0c06e7b29fb70c1901df3f10ef71f9
Doctor moeclay <moeamarreza@gmail.com>
2019-10-16 11:52:05 +0700

    Add files via upload

---
+++ 01_permulaan/02_web_server.js
@@ -0,0 +1,8 @@
+var http = require('http');
+
+var server = http.createServer(function(req, res){
+  res.end("Hi, selamat datang di nodejs");
+});
+
+server.listen(8000);
+console.log("server running on http://localhost:8000");
